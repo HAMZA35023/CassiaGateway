62d1be65419b964ebc304c6f10c3bd7c1fadb91f
Hamza Rashid <Hamza.Rashid@niko.eu>
2024-10-28 09:11:02 +0100

    random models

---
+++ AccessAPP/Models/FirmwareResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessAPP.Models
+{
+    public class FirmwareResponseModel
+    {
+        public string Status {  get; set; }
+        public string Message { get; set; }
+
+        public object Data { get; set; }    
+    }
+}
---
+++ AccessAPP/Models/FirmwareUpgradeResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace AccessAPP.Models
+{
+    public class FirmwareUpgradeResult
+    {
+        public string MacAddress { get; set; }
+        public string Status { get; set; }
+        public HttpStatusCode ErrorCode { get; set; }
+        public string Message { get; set; }
+    }
+}
